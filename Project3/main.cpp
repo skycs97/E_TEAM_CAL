d496f105a50aab13903b436ec42cb46279ace578
dongig.shin <dongig.shin@samsung.com>
2025-07-01 15:07:40 +0900

    update 4.getMinus

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,10 +3,19 @@
 class Cal {
 public:
 	//Code space
+	int getMinus(int a, int b) {
+		return a - b;
+	}
 };
 
 //Test Case
-
+TEST(TEST, getMinus) {
+	Cal c;
+	EXPECT_EQ(1, c.getMinus(3, 2));
+	EXPECT_EQ(2, c.getMinus(4, 2));
+	EXPECT_EQ(3, c.getMinus(5, 2));
+	EXPECT_EQ(4, c.getMinus(6, 2));
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
