fe4696b6080bca7db08b7087097952e206ec7a81
jieun <amykim0116@naver.com>
2025-07-01 15:06:13 +0900

    zegop enable

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,10 +3,30 @@
 class Cal {
 public:
 	//Code space
+	int zegop(int a) { return a * a; }
+
 };
 
 //Test Case
+TEST(KJE, Zegop1)
+{
+	Cal cal;
+	int a = 10;
+	int expected = 100;
+	int result = cal.zegop(a);
 
+	EXPECT_EQ(expected, result);
+}
+
+TEST(KJE, Zegop2)
+{
+	Cal cal;
+	int a = -10;
+	int expected = 100;
+	int result = cal.zegop(a);
+
+	EXPECT_EQ(expected, result);
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
