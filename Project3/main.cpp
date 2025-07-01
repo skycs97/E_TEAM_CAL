1f0167fc637f455348a155ae475b1cfeff1075f9
MJ <julysky40@gmail.com>
2025-07-01 15:03:06 +0900

    Cal::getSum 구현

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,10 +3,21 @@
 class Cal {
 public:
 	//Code space
+	int getSum(int a, int b)
+	{
+		return a + b;
+	}
+
 };
 
 //Test Case
-
+TEST(Caltest, getSumTest) {
+	Cal cal;
+	EXPECT_EQ(0, cal.getSum(0, 0));
+	EXPECT_EQ(5, cal.getSum(0, 5));
+	EXPECT_EQ(7, cal.getSum(7, 0));
+	EXPECT_EQ(13, cal.getSum(3, 10));
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
