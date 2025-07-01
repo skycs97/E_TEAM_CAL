33c7441c19d6e2076c8fa71071a8740fb626fe72
INHO <choo4r@gmail.com>
2025-07-01 15:06:40 +0900

    add Divide function

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,10 +1,31 @@
 #include "gmock/gmock.h"
 
+using namespace testing;
+
 class Cal {
 public:
-	//Code space
+	int getDivide (int a, int b) {
+		if (b == 0) {
+			return 0x7FFFFFFF; // Return a large number to indicate an error
+		}
+		return a / b;
+	}
 };
 
+TEST(CalTest, DivideByZero) {
+	Cal cal;
+	EXPECT_EQ(cal.getDivide(10, 0), 0x7FFFFFFF);
+}
+
+TEST(CalTest, DivideByNonZero) {
+	Cal cal;
+	EXPECT_EQ(cal.getDivide(10, 2), 5);
+	EXPECT_EQ(cal.getDivide(10, 1), 10);
+	EXPECT_EQ(cal.getDivide(10, -2), -5);
+	EXPECT_EQ(cal.getDivide(-10, 2), -5);
+	EXPECT_EQ(cal.getDivide(-10, -2), 5);
+	EXPECT_EQ(cal.getDivide(0, -2), 0);
+}
 //Test Case
 
 
