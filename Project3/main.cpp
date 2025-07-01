58dc27687393e371a4427f046dc6e5e45f655863
skycs97 <skycs97@gmail.com>
2025-07-01 15:12:04 +0900

    add MAC operator for Cal class

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,10 +3,21 @@
 class Cal {
 public:
 	//Code space
+	int getMultiplyAccumulate(int a, int b, int c) {
+		return (a * b) + c;
+	}
 };
 
 //Test Case
+TEST(CAL_TEST, MAC_TC_1) {
+	int a = 3;
+	int b = 5;
+	int c = 2;
 
+	Cal cal;
+
+	EXPECT_EQ(17, cal.getMultiplyAccumulate(a, b, c));
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
