1b5f9dd1e48ce53cc256fb2d26e5f5cbeb3c29a6
soonwonchoi <soonwon.choi@samsung.com>
2025-07-01 15:06:29 +0900

    getGop

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,9 +3,18 @@
 class Cal {
 public:
 	//Code space
+	int getGop(int a, int b)
+	{
+		return a * b;
+	}
 };
 
 //Test Case
+TEST(TS, TC_Gop)
+{
+	Cal cal;
+	EXPECT_EQ(10, cal.getGop(2, 5));
+}
 
 
 int main(void) {
