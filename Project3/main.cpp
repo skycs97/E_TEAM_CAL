1322556f3897ad401b6ed5baf6ae069b759500e9
YoungJae Ryu <zeroexit@gmail.com>
2025-07-01 15:14:43 +0900

    Add getSumSum feature

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,12 +1,19 @@
 #include "gmock/gmock.h"
 
+using namespace testing;
+
 class Cal {
 public:
 	//Code space
+	int getSumSum(int a, int b, int c) { return a + b + c; }
 };
 
 //Test Case
+TEST(CalTest, t6) {
 
+	Cal cal;
+	EXPECT_EQ(10, cal.getSumSum(2, 3, 5));
+}
 
 int main(void) {
 	::testing::InitGoogleMock();
